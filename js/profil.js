a0d1cec434b694a86b106a92fd995808011976e7
NolanCouny <nolancouny28240@gmail.com>
2026-07-21 17:43:04 +0200

    Création de la page profil

---
+++ js/profil.js
@@ -0,0 +1,14 @@
+// Gestion de la page profil
+
+const editButton = document.getElementById("edit-profile");
+
+
+if(editButton){
+
+    editButton.addEventListener("click", () => {
+
+        alert("Modification du profil bientôt disponible");
+
+    });
+
+}
