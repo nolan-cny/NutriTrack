2a1668bcef288ca1872ce176f7683b74269f68e6
NolanCouny <nolancouny28240@gmail.com>
2026-07-22 12:24:27 +0200

    modifications de firebase

--- js/firebase.js
+++ js/firebase.js
@@ -1,38 +1,53 @@
-// Import des modules Firebase depuis le CDN
+// Firebase
 
-import { initializeApp } from 
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
-import { 
-    getAuth 
-} from 
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAuth } from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-import { 
-    getFirestore 
-} from 
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getFirestore } from
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 
-// Import de la configuration
-import { firebaseConfig } from "./config.js";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDCEF70xvj3DsCWuONIqyC4dq38OQ0eScA",
+
+    authDomain: "nutritrack-v3.firebaseapp.com",
+
+    projectId: "nutritrack-v3",
+
+    storageBucket: "nutritrack-v3.firebasestorage.app",
+
+    messagingSenderId: "789592608860",
+
+    appId: "1:789592608860:web:c5135f9b2ae2ab7dd21699",
+
+    measurementId: "G-RQP7M0BR8Z"
+
+};
 
 
-// Initialisation Firebase
+// Initialisation
+
 const app = initializeApp(firebaseConfig);
 
 
-// Initialisation Authentication
+// Services Firebase
+
 const auth = getAuth(app);
 
-
-// Initialisation Firestore
 const db = getFirestore(app);
 
 
-// Export pour les autres fichiers
+// Export
+
 export {
-    app,
+
     auth,
+
     db
+
 };
